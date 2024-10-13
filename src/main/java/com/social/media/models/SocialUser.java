8ef74f9d1cc9acf6fdca228244a6fcce4d6e77e4
Gowshik <koushikraj60@gmail.com>
2024-10-13 18:13:45 +0530

    many-one-many-relationship

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,13 +1,19 @@
 package com.social.media.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(mappedBy = "user")
+    private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<Post> posts = new ArrayList<>();
 }
--- src/test/java/com/social/media/MediaApplicationTests.java
+++ src/test/java/com/social/media/MediaApplicationTests.java
@@ -8,6 +8,7 @@ class MediaApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
